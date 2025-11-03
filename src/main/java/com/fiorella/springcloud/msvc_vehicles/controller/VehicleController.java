b496155122dda71397fa1d273752948206edc387
fgalarzad <i202330589@cibertec.edu.pe>
2025-11-03 00:00:59 -0500

    Circuit breaker

--- src/main/java/com/fiorella/springcloud/msvc_vehicles/controller/VehicleController.java
+++ src/main/java/com/fiorella/springcloud/msvc_vehicles/controller/VehicleController.java
@@ -2,10 +2,9 @@ package com.fiorella.springcloud.msvc_vehicles.controller;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,27 +17,7 @@ import com.fiorella.springcloud.msvc_vehicles.service.VehicleService;
 @RestController
 @RequestMapping("/api/vehicles")
 public class VehicleController {
-/*
-   @Value("${server.port}")
-    private int port;
 
-    @GetMapping
-    public List<Vehicle> listVehicles() {
-        List<Vehicle> vehicles = List.of(
-                new Vehicle(1L, "ABC-123", "Toyota", "Corolla", "ENG-001", 5, 0),
-                new Vehicle(2L, "XYZ-456", "Honda", "Civic", "ENG-002", 5, 0),
-                new Vehicle(3L, "JKL-789", "Nissan", "Sentra", "ENG-003", 5, 0),
-                new Vehicle(4L, "MNO-321", "Ford", "Ranger", "ENG-004", 2, 0)
-        );
-
-        // Asignar el puerto actual para distinguir instancias
-        return vehicles.stream()
-                .map(v -> {
-                    v.setPort(port);
-                    return v;
-                })
-                .collect(Collectors.toList());
-    }*/
 
     @Autowired
     private VehicleService vehicleService;
@@ -49,7 +28,16 @@ public class VehicleController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Vehicle> detail(@PathVariable Long id) {
+    public ResponseEntity<Vehicle> detail(@PathVariable Long id) throws InterruptedException{
+        
+        if (id.equals(5L)){
+            throw new IllegalStateException("Vehiculo no encontrados");
+
+        }
+        if (id.equals(2L)) {
+            TimeUnit.SECONDS.sleep(1L);
+        }
+        
         Optional<Vehicle> optional = vehicleService.findById(id);
         if (optional.isPresent()) {
             return ResponseEntity.ok(optional.get());
